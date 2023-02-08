c629d4b0e0ebc4416e6376f02ac805730d5f1463
Elsen01 <elsen@gmail.com>
2023-02-08 23:39:59 +0400

    list-product

---
+++ frontend/src/data.js
@@ -0,0 +1,58 @@
+const data  = {
+    products : [
+        {
+        
+            name: 'Nike Slim Shirt',
+            slug: 'nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 150,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt'
+    },{
+
+            name: 'Adidas Slim Shirt',
+            slug: 'adidas Slim Shirt',
+            category: 'Shirts',
+            image: '/images/d2.jpg',
+            price: 110,
+            countInStock: 12,
+            brand: 'Adidas',
+            rating: 4.6,
+            numReviews: 12,
+            description: 'high quality shirt'
+        },
+        {
+
+            name: 'Adidas Fit Pant',
+            slug: 'adidas-fit-pant',
+            category: 'Pants',
+            image: '/images/p2.jpg',
+            price: 100,
+            countInStock: 12,
+            brand: 'Adidas',
+            rating: 4.0,
+            numReviews: 5,
+            description: 'high quality product'
+        },
+        
+        {
+
+            name: 'Puma Slim Pants',
+            slug: 'puma-slim-pants',
+            category: 'Pants',
+            image: '/images/p3.jpg',
+            price: 120,
+            countInStock: 15,
+            brand: 'Nike',
+            rating: 4.2,
+            numReviews: 7,
+            description: 'high quality product'
+        },
+    ],
+};
+
+export default data;

9b27061269c0b9526e43c53830d5a9aa519049bf
Elsen01 <elsen@gmail.com>
2023-02-10 01:34:42 +0400

    Fetch-Product-From-Backend

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,13 @@ import express from 'express';
 import data from './data.js'
 
 const app = express();
-
+//test
 app.get('/api/products',(req,res) => {
     res.send(data.products)
 });
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 7000;
 
 app.listen(port,() => {
-    console.log(`Listening on ${port}`)
+    console.log(`http://localhost${port}`)
 });
